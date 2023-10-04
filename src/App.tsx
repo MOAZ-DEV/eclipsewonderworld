4532fdb81674bb26e5c33fae614f30dd5a95d450
Moaz Alla Elden <91629974+MOAZ-DEV@users.noreply.github.com>
2023-10-04 12:29:58 +0300

    Last Pizza :pizza:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage.tsx";
-import './App.scss'
-import { EclipseTypesPage } from "./pages/EclipseTypesPage.tsx";
-import { YouAreLost } from "./pages/YouAreLost.tsx";
-import { EclipsesThroughoutHistory } from "./pages/EclipsesThroughoutHistory.tsx";
+import { Outlet } from "react-router-dom";
 
+import './App.scss'
 const App = () => {
 
-  return <BrowserRouter>
-
-    <Routes>
-<Route path="/eclipsewonderworld/" element={<HomePage />} >
-         <Route path="*" element={<YouAreLost />} />
-        <Route path="eclipse_types" element={<EclipseTypesPage />} />
-        <Route path="eclipses_throughout_history" element={<EclipsesThroughoutHistory />} />
-</Route>
-
-    </Routes>
-
-  </BrowserRouter>;
+  return <Outlet />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HomePage } from './pages/HomePage.tsx'
+import { YouAreLost } from './pages/YouAreLost.tsx'
+import { EclipseTypesPage } from './pages/EclipseTypesPage.tsx'
+import { EclipsesThroughoutHistory } from './pages/EclipsesThroughoutHistory.tsx'
 
+const router = createBrowserRouter([{
+  path: "/eclipsewonderworld/",
+  element: <App />,
+  children: [
+    {      path: "/eclipsewonderworld/*", element: <YouAreLost />},
+    {      path: "/eclipsewonderworld/", element: <HomePage />},
+    {      path: "/eclipsewonderworld/home", element: <HomePage />},
+    {      path: "/eclipsewonderworld/eclipse_types", element: <EclipseTypesPage />},
+    {      path: "/eclipsewonderworld/eclipses_throughout_history", element: <EclipsesThroughoutHistory />},
+  ]
+}])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider router={router}/>
+
   </React.StrictMode>,
 )
