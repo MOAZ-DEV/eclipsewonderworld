634f16013e8628cdc0b14a253d6ef4bcf0db31df
Moaz Alla Elden <91629974+MOAZ-DEV@users.noreply.github.com>
2023-10-08 07:46:26 +0300

    Update PNL.tsx

--- src/components/PNL.tsx
+++ src/components/PNL.tsx
@@ -26,7 +26,7 @@ export const PNL = () => <div className="pnlWrap">
                 Eclipse matching</h5>
             <h6 className="BM12">
                 A memory game where kids are shown a series of images and asked to match them up</h6></a></li>
-        <li><a href="https://mohammedhamdy102003.github.io/puzzle/">
+        <li><a href="https://mohammedhamdy102003.github.io/puzzle3/">
             <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0.5 13L12.5 1M12.5 1H1.5M12.5 1V11" stroke="black" /></svg>
             <h5 className="BB18">
